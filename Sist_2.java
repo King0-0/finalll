d0575b96da1e5d1d1aaf3603dd1aa270b86d087d
sangwon0707 <sangwon07@gmail.com>
2024-03-19 12:51:44 +0900

    sist_2상원

---
+++ Sist_2.java
@@ -0,0 +1,12 @@
+class SangwonDAO{
+	public void insert(){
+		System.out.println("sangwon01");
+		System.out.println("sangwon02");
+	}
+	public List list(){
+		ArrayList list = new ArrayList();
+		list.add("1번 sangwon리스트");
+		list.add("2번 sangwon리스트");
+		return list;
+	}
+}
