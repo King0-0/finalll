317a1533dbda6e16d13677af8bde31b09ec3fcce
jusunKim <155524612+jusunKim@users.noreply.github.com>
2024-03-19 12:35:07 +0900

    jusunDAO 완성[김주선]

---
+++ sist_3.java
@@ -0,0 +1,12 @@
+class jusunDAO{
+	public void insert(){
+		System.out.println("jusun1");
+		System.out.println("jusun2");
+	}
+	public List list(){
+		ArrayList list = new ArrayList();
+		list.add("1번 jusun게시물");
+		list.add("2번 jusun게시물");
+		return list;
+	}
+}
